8b270f1a378527cc4d7588b006da4ac70bccd4d7
a940731 <a940731@DESKTOP-KT3CS8K>
2025-03-31 10:54:54 +0200

    Terminado el ejercicio

--- Animalia/src/main/java/es/springcore/animalia/Main.java
+++ Animalia/src/main/java/es/springcore/animalia/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		// Leer la clase de configuracion
 		AnnotationConfigApplicationContext contexto = new AnnotationConfigApplicationContext(AnimalesConfig.class);
 		
